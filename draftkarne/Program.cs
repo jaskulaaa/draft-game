6f4b67bc830fd9007e29ca17dc090cb641455e8a
Your Name <skibidier>
2024-11-12 08:43:38 +0100

    plik z kodem

---
+++ draftkarne/Program.cs
@@ -0,0 +1,58 @@
+﻿public class Zawodnik
+{
+    public string imienazwisko;
+    public int atk;
+    public int obr;
+    public int kar;
+    public string poz;
+
+
+    public Zawodnik(string nickname, int atack, int def, int penalty, string position)
+    {
+        imienazwisko = nickname;
+        atk = atack;
+        obr = def;
+        kar = penalty;
+        poz = position;
+    }
+}
+class Gra
+{
+    
+
+    private List<Zawodnik> zawodnicy;
+    private List<Zawodnik> skladGracza1;
+    private List<Zawodnik> skladGracza2;
+
+    public Gra()
+    {
+        zawodnicy = new List<Zawodnik>
+        {
+            new Zawodnik("Jan Kowalski", 0, 90, 75, "Bramkarz"),
+   
+           
+        };
+    }
+
+    public void RozpocznijDraft()
+    {
+        // Logika draftu, wybór zawodników przez obu graczy
+    }
+
+    public void SymulujMecz()
+    {
+        // Logika symulacji meczu, generowanie wyniku
+        Console.WriteLine($"Wynik meczu: {wynikGracza1} - {wynikGracza2}");
+
+        // Rzuty karne
+        RzutyKarne();
+    }
+
+    public void RzutyKarne()
+    {
+        // Logika wykonywania rzutów karnych
+    }
+}
+
+
+
