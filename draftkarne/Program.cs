cd67d27165df83d96b3999fe57f661e79f608ea4
Dominik <dominik.konieczny@uczen.zsk.poznan.pl>
2024-11-14 10:31:31 +0100

    SOME PROGRESS IN POBIERANIE DANYCH Z PLIKU

--- draftkarne/Program.cs
+++ draftkarne/Program.cs
@@ -1,11 +1,15 @@
-﻿public class Zawodnik
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+public class Zawodnik
 {
     public string imienazwisko;
     public int atk;
     public int obr;
     public int kar;
     public string poz;
-
 
     public Zawodnik(string nickname, int atack, int def, int penalty, string position)
     {
@@ -16,22 +20,50 @@
         poz = position;
     }
 }
+
 class Gra
 {
-    
-
-    private List<Zawodnik> zawodnicy;
-    private List<Zawodnik> skladGracza1;
-    private List<Zawodnik> skladGracza2;
+    private List<Zawodnik> zawodnicy;  // Lista do przechowywania zawodników
 
     public Gra()
     {
-        zawodnicy = new List<Zawodnik>
+        zawodnicy = new List<Zawodnik>();
+        WczytajZawodnikowZPliku(".zawodnicytxt");
+    }
+
+    public void WczytajZawodnikowZPliku(string sciezkaPliku)
+    {
+        try
         {
-            new Zawodnik("Jan Kowalski", 0, 90, 75, "Bramkarz"),
-   
-           
-        };
+            // Otwórz plik do odczytu
+            string[] linie = File.ReadAllLines(sciezkaPliku);
+
+            // Przetwarzaj każdą linię
+            foreach (string linia in linie)
+            {
+                // Podziel linię na części, zakładając, że dane są oddzielone przecinkami
+                string[] dane = linia.Split(',');
+
+                if (dane.Length == 5)  // Sprawdź, czy linia zawiera poprawną liczbę danych
+                {
+                    string imienazwisko = dane[0].Trim();
+                    int atk = int.Parse(dane[1].Trim());
+                    int obr = int.Parse(dane[2].Trim());
+                    int kar = int.Parse(dane[3].Trim());
+                    string poz = dane[4].Trim();
+
+                    // Utwórz obiekt Zawodnik i dodaj do listy
+                    Zawodnik zawodnik = new Zawodnik(imienazwisko, atk, obr, kar, poz);
+                    zawodnicy.Add(zawodnik);
+                }
+            }
+
+            Console.WriteLine("Zawodnicy zostali wczytani z pliku.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Wystąpił błąd podczas wczytywania pliku: {e.Message}");
+        }
     }
 
     public void RozpocznijDraft()
@@ -42,7 +74,7 @@ class Gra
     public void SymulujMecz()
     {
         // Logika symulacji meczu, generowanie wyniku
-        Console.WriteLine($"Wynik meczu: {wynikGracza1} - {wynikGracza2}");
+        //  Console.WriteLine($"Wynik meczu: {wynikGracza1} - {wynikGracza2}");
 
         // Rzuty karne
         RzutyKarne();
@@ -51,8 +83,7 @@ class Gra
     public void RzutyKarne()
     {
         // Logika wykonywania rzutów karnych
+        // Ostateczny wynik
     }
 }
-
-
 
--- draftkarne/obj/Release/net8.0/draftkarne.AssemblyInfo.cs
+++ draftkarne/obj/Release/net8.0/draftkarne.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("draftkarne")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f4b67bc830fd9007e29ca17dc090cb641455e8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("draftkarne")]
