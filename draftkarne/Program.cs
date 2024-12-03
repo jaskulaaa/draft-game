56324318ea120c61d0b333a75cdf96c873696cfa
kacper <kacperjaskula2005@gmail.com>
2024-12-03 22:49:31 +0100

    zmienia kod na czytelniejsz i dodaje funkcje ze zmiennymi do losowania i wybiernai by ulatwic czytelnosc

--- draftkarne/Program.cs
+++ draftkarne/Program.cs
@@ -1,9 +1,6 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
 using System.IO;
-using System.Text;
 
 public class Zawodnik
 {
@@ -22,64 +19,57 @@ public class Zawodnik
         poz = position;
     }
 }
-    public class Sklad1
+
+public class Sklad1
+{
+    public string imienazwisko;
+    public int atk;
+    public int obr;
+    public int kar;
+    public string poz;
+
+    public Sklad1(string nickname, int atack, int def, int penalty, string position)
     {
-        public string imienazwisko;
-        public int atk;
-        public int obr;
-        public int kar;
-        public string poz;
-
-        public Sklad1(string nickname, int atack, int def, int penalty, string position)
-        {
-            imienazwisko = nickname;
-            atk = atack;
-            obr = def;
-            kar = penalty;
-            poz = position;
-        }
+        imienazwisko = nickname;
+        atk = atack;
+        obr = def;
+        kar = penalty;
+        poz = position;
     }
-
+}
 
 class Gra
-{   
-    private List<Zawodnik> zawodnicy; 
-    private List<Sklad1> sklad1;  // Lista do przechowywania zawodników
+{
+    private List<Zawodnik> zawodnicy;
+    private List<Sklad1> sklad1; // Lista składu gracza
     public string nazwaGracza1;
     public string nazwaGracza2;
-    
 
     public Gra()
     {
         zawodnicy = new List<Zawodnik>();
-        WczytajZawodnikowZPliku("C:\\Users\\student\\Desktop\\mango\\draft-game\\draftkarne\\pilakrze.txt");
-        Console.WriteLine("Gracz 1 podaj swoja nazwe");
+        WczytajZawodnikowZPliku("C:\\Users\\kacpe\\Desktop\\draft\\draft-game\\draftkarne\\pilakrze.txt");
+        Console.WriteLine("Gracz 1 podaj swoją nazwę:");
         nazwaGracza1 = Console.ReadLine();
-        Console.WriteLine("Gracz 2 podaj swoja nazwe");
+        Console.WriteLine("Gracz 2 podaj swoją nazwę:");
         nazwaGracza2 = Console.ReadLine();
         Console.WriteLine("Rozpoczęcie gry pomiędzy " + nazwaGracza1 + " vs " + nazwaGracza2);
-            
+
         sklad1 = new List<Sklad1>();
         RozpocznijDraft();
-
     }
-
-    
 
     public void WczytajZawodnikowZPliku(string sciezkaPliku)
     {
         try
         {
-           
             string[] linie = File.ReadAllLines(sciezkaPliku);
 
-         
             foreach (string linia in linie)
             {
-                
                 string[] dane = linia.Split(',');
 
-                if (dane.Length == 5)  // Sprawdź, czy linia zawiera poprawną liczbę danych
+                if (dane.Length == 5)
                 {
                     string imienazwisko = dane[0].Trim();
                     int atk = int.Parse(dane[1].Trim());
@@ -87,7 +77,6 @@ class Gra
                     int kar = int.Parse(dane[3].Trim());
                     string poz = dane[4].Trim();
 
-                    
                     Zawodnik zawodnik = new Zawodnik(imienazwisko, atk, obr, kar, poz);
                     zawodnicy.Add(zawodnik);
                 }
@@ -103,152 +92,95 @@ class Gra
 
     public void RozpocznijDraft()
     {
-        Console.WriteLine("Rozpoczęcie budowy skłądu " + nazwaGracza1);
-        
+        Console.WriteLine("Rozpoczęcie budowy składu " + nazwaGracza1);
+
         Random losowe = new Random();
-        string[] mozlwibramkarze = new string[3]; 
+        string[] mozlwibramkarze = new string[3];
         string[] mozlwiobroncy = new string[6];
         string[] mozlwipomocnicy = new string[9];
         string[] mozlwinapastnicy = new string[6];
-        for (int i = 0; i < mozlwibramkarze.Length; i++)
+
+        // Losowanie Bramkarzy
+        LosujZawodnikow(mozlwibramkarze, 76, 100, losowe);
+
+        // Losowanie Obrońców
+        LosujZawodnikow(mozlwiobroncy, 56, 75, losowe);
+
+        // Losowanie Pomocników
+        LosujZawodnikow(mozlwipomocnicy, 31, 55, losowe);
+
+        // Losowanie Napastników
+        LosujZawodnikow(mozlwinapastnicy, 1, 30, losowe);
+
+        // Wybór zawodników
+        WybierzZawodnikow(mozlwibramkarze, "Bramkarz", 1);
+        WybierzZawodnikow(mozlwiobroncy, "Obrońca", 2);
+        WybierzZawodnikow(mozlwipomocnicy, "Pomocnik", 3);
+        WybierzZawodnikow(mozlwinapastnicy, "Napastnik", 2);
+
+        // Wyświetlenie składu
+        Console.WriteLine("Twój skład:");
+        foreach (var zawodnik in sklad1)
         {
-            bool spr = false;
-
-            mozlwibramkarze[i] = zawodnicy[losowe.Next(76,100)].imienazwisko;
-
-            foreach(string bram in mozlwibramkarze)
-            {
-                if(mozlwibramkarze [i] == bram)
-                {
-                    spr = true;
-                }
-            }
-            if (spr == true){
-
-            }
-           
-          
+            Console.WriteLine($"{zawodnik.poz}: {zawodnik.imienazwisko} (Atk: {zawodnik.atk}, Obr: {zawodnik.obr}, Kar: {zawodnik.kar})");
         }
-        for (int i = 0; i < mozlwiobroncy.Length; i++)
-        {
-            bool spr = false;
-
-            mozlwiobroncy[i] = zawodnicy[losowe.Next(56, 75)].imienazwisko;
-
-            foreach (string ob in mozlwiobroncy)
-            {
-                if (mozlwiobroncy[i] == ob)
-                {
-                    spr = true;
-                }
-            }
-            if (spr == true)
-            {
-
-            }
-
-
-        }
-        for (int i = 0; i < mozlwipomocnicy.Length; i++)
-        {
-            bool spr = false;
-
-            mozlwipomocnicy[i] = zawodnicy[losowe.Next(30, 55)].imienazwisko;
-
-            foreach (string pom in mozlwipomocnicy)
-            {
-                if (mozlwipomocnicy[i] == pom)
-                {
-                    spr = true;
-                }
-            }
-            if (spr == true)
-            {
-
-            }
-
-
-        }
-        for (int i = 0; i < mozlwinapastnicy.Length; i++)
-        {
-            bool spr = false;
-
-            mozlwinapastnicy[i] = zawodnicy[losowe.Next(30, 55)].imienazwisko;
-
-            foreach (string nap in mozlwinapastnicy)
-            {
-                if (mozlwinapastnicy[i] == nap)
-                {
-                    spr = true;
-                }
-            }
-            if (spr == true)
-            {
-
-            }
-
-
-        }
-        Console.WriteLine("Wybierz jednego z 3 Bramkarzy podając numer od 1 do 3");
-        foreach (string bram in mozlwibramkarze )
-        {
-            Console.WriteLine (bram);
-        }
-        int wybor = int.Parse(Console.ReadLine());
-        if (wybor > 3 || wybor < 1)
-        {
-            Console.WriteLine("Prosze wybrac numer od 1 do 3");
-        }
-        else
-        {
-            string wybranybram = mozlwibramkarze[wybor-1];
-            Console.WriteLine("wybrałeś Bramkarza: " + wybranybram);
-        }
-        Console.WriteLine("Wybierz dwoch z 6 oobroncow podając dwa różne numery od 1 do 6");
-        foreach(string bram in mozlwiobroncy)
-        {
-            Console.WriteLine(bram);
-        }
-        int wybor1 = int.Parse(Console.ReadLine());
-        int wybor2 = int.Parse(Console.ReadLine());
-        if ((wybor1 > 6 || wybor1 < 1)|| (wybor2 > 6 || wybor2 < 1) || (wybor == wybor2))
-        {
-            Console.WriteLine("Prosze wybrac numer od 1 do 6 i nie podawaj dwa razy tego samegu numeru");
-        }
-        else
-        {
-            string wybranyobr = mozlwiobroncy[wybor1-1];
-            string wybranyobr2 = mozlwiobroncy[wybor2-1];
-            Console.WriteLine("wybrałeś Obrońcó: " + wybranyobr + " i " + wybranyobr2);
-        }
-
-
-        // Logika draftu, wybór zawodników przez obu graczy
-
     }
 
-    public void SymulujMecz()
+    private void LosujZawodnikow(string[] lista, int minmalnyindex, int maksymlanyindex, Random losowe)
     {
-        // Logika symulacji meczu, generowanie wyniku
-      
+        for (int i = 0; i < lista.Length; i++)
+        {
+            bool spr;
+            do
+            {
+                spr = false;
+                lista[i] = zawodnicy[losowe.Next(minmalnyindex, maksymlanyindex)].imienazwisko;
 
-        // Rzuty karne
-        RzutyKarne();
+                for (int j = 0; j < i; j++)
+                {
+                    if (lista[i] == lista[j])
+                    {
+                        spr = true;
+                        break;
+                    }
+                }
+            } while (spr);
+        }
     }
 
-    public void RzutyKarne()
+    private void WybierzZawodnikow(string[] lista, string pozycja, int liczbaDoWyboru)
     {
-        // Logika wykonywania rzutów karnych
-        // Ostateczny wynik
+        Console.WriteLine($"Wybierz {liczbaDoWyboru} z {lista.Length} {pozycja.ToLower()}(ów) podając odpowiednie numery:");
+
+        for (int i = 0; i < lista.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}: {lista[i]}");
+        }
+
+        List<int> wybrane = new List<int>();
+        while (wybrane.Count < liczbaDoWyboru)
+        {
+            int wybor = int.Parse(Console.ReadLine());
+            if (wybor < 1 || wybor > lista.Length || wybrane.Contains(wybor))
+            {
+                Console.WriteLine("Niepoprawny wybór! Spróbuj ponownie.");
+            }
+            else
+            {
+                wybrane.Add(wybor);
+                string wybranyZawodnik = lista[wybor - 1];
+                Zawodnik zawodnik = zawodnicy.Find(z => z.imienazwisko == wybranyZawodnik);
+                if (zawodnik != null)
+                {
+                    sklad1.Add(new Sklad1(zawodnik.imienazwisko, zawodnik.atk, zawodnik.obr, zawodnik.kar, pozycja));
+                    Console.WriteLine($"{zawodnik.imienazwisko} został dodany do Twojego składu!");
+                }
+            }
+        }
     }
 
     public static void Main()
     {
-       
         Gra gra = new Gra();
     }
-
 }
-
-
-
--- draftkarne/obj/Debug/net8.0/draftkarne.AssemblyInfo.cs
+++ draftkarne/obj/Debug/net8.0/draftkarne.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("draftkarne")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f799a44d5bcc2e87b5efe395d2221034047f9e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8a08232f031106f3e3c688f8d5f44482101ebf7")]
 [assembly: System.Reflection.AssemblyProductAttribute("draftkarne")]
 [assembly: System.Reflection.AssemblyTitleAttribute("draftkarne")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
